d69d5cb04da5d8c662a2ce494f405e1d723d05de
Jeffrey Minton <jeffrey.minton@gmail.com>
2011-02-15 11:33:59 -0500

    redoing the lib after losing them

---
+++ util.cpp
@@ -0,0 +1,58 @@
+/**
+Programmer:		Jeffrey Minton
+File:			util.cpp
+Description:	A file containing various utilities
+*/
+
+
+#include "util.h"
+
+
+//match
+bool Util::match(string str, string pattern)
+{
+	regex regPattern = regex(pattern);
+
+	return regex_match(str, regPattern);
+}
+
+
+//getMatch
+string Util::getMatch(string str, string pattern, int grp)
+{
+	regex regPattern = regex(pattern);
+	smatch regOut;
+
+	if(regex_match(str, regOut, regPattern) == false)
+	{
+		return "";
+	}
+
+	return regOut[grp];
+}
+
+
+//split
+vector<string> Util::split(string str, string pattern)
+{
+	vector<string> out;
+	regex splitReg = regex(pattern);
+
+	const sregex_token_iterator end;
+
+	for(sregex_token_iterator i(str.begin(), str.end(), splitReg); i != end; i++)
+	{
+		out.push_back(*i);
+	}
+
+	return out;
+}
+
+
+//strip
+void Util::strip(string &str)
+{
+	str = getMatch(str, STRIPLINE, 1);
+
+	return;
+}
---
+++ util.h
@@ -0,0 +1,68 @@
+/**
+Programmer:		Jeffrey Minton
+File:			util.h
+Description:	A file containing various utilities
+*/
+
+
+#include <regex>
+#include <vector>
+#include <string>
+
+#define STRIPLINE "^\\s*(.*)\\s$"
+#define IGNORE_TAG "<\\?.*\\?>"
+#define TAG "^<.*>$"
+#define CLOSE_TAG "^</.*>$"
+#define CLOSE_TAG_NAME "</(\\w+)"
+#define ONE_LINE_TAG "^<.+/>$"
+#define ONE_LINE_OPEN_CLOSE "(<.*>)(.+)(</.*>)"
+#define OPEN_TAG_NAME "<(\\w+)"
+#define ATTRIBUTE "\\w+\\s*=\\s*\\w"
+#define ATTR_SPLIT "(\\w+)\\s*=\\s*(\\w+)"
+
+using namespace std;
+
+class Util
+{
+public:
+	/**
+	Function:		match
+	Description:	Determines if a string matches a regex
+	Parameters:		string str - the string ti check
+					string pattern - the pattern to search for
+	Return:			bool - true matches, false, no match
+	*/
+	bool match(string str, string pattern);
+
+
+	/**
+	Function:		getMatch
+	Description:	return the substring from a string that
+					matches the given group of a regex
+	Parameters:		string str - the string to match
+					string pattern - the pattern to match
+					int grp - the group of the regex to return
+	Return:			string - the sub string representing the group
+					null if not found
+	*/
+	string getMatch(string str, string pattern, int grp);
+
+	/**
+	Function:		split
+	Description:	return a vector containing all items in a string
+					that match a give regex
+	Parameters:		string str - the string to split
+					string pattern - the regex patter to split on
+	Return:			vector<string> - vector containing items
+	*/
+	vector<string> split(string str, string pattern);
+
+	/**
+	Function:		strip
+	Description:	remove the leading and trailing white space from
+					a string
+	Parameters:		string str - the string to strip
+	*/
+	void strip(string &str);
+};
+
